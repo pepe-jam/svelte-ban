24722effb1f11b96fe2b4162f0e711db63e31536
Pascallo <pascal.wegener@student.hs-anhalt.de>
2023-03-05 21:41:17 +0100

    container added to main +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,58 +25,67 @@
   }
 </script>
 
-{#if ready}
-  <div class="title">
-    <img src={bananoLogo} class="logo banano" alt="Banano Logo" />
-    <div>
-      <div class="subtitle">did you know?</div>
-      <div>1 Ban = 1 Ban</div>
+<div class="container">
+  {#if ready}
+    <div class="title">
+      <img src={bananoLogo} class="logo banano" alt="Banano Logo" />
+      <div>
+        <div class="subtitle">did you know?</div>
+        <div>1 Ban = 1 Ban</div>
+      </div>
+      <img src={bananoLogo} class="logo banano" alt="Banano Logo" />
     </div>
-    <img src={bananoLogo} class="logo banano" alt="Banano Logo" />
-  </div>
 
-  <MonkeyCard {account} {validAccount} />
-  <AddressSearch bind:account {transactionHistory} {validAccount} />
-  {#if transactionHistory}
-    <MonkeyAccount {bananodeApi} />
+    <AddressSearch bind:account {transactionHistory} {validAccount} />
+    {#if transactionHistory}
+      <MonkeyAccount {bananodeApi} />
+    {/if}
+    <TransactionHistory bind:this={transactionHistory} {bananodeApi} />
+    <MonkeyCard {account} {validAccount} />
   {/if}
-  <TransactionHistory bind:this={transactionHistory} {bananodeApi} />
-{/if}
+</div>
 
 <style lang="scss">
-  .subtitle {
-    font-size: 0.6em;
-    letter-spacing: 0.15em;
-    text-align: center;
-  }
-
-  .title {
+  .container {
     display: flex;
-    flex-flow: row;
+    flex-direction: column;
     align-items: center;
-    font-size: 1.2em;
-    letter-spacing: 0.05em;
+    margin-bottom: 2em;
 
-    .logo {
-      width: 2.3em;
-      padding: 1em;
-      vertical-align: middle;
+    .subtitle {
+      font-size: 0.6em;
+      letter-spacing: 0.15em;
+      text-align: center;
     }
-  }
 
-  @media (min-width: 420px) {
     .title {
-      font-size: 1.4em;
-    }
-  }
-
-  @media (min-width: 576px) {
-    .title {
-      font-size: 2em;
+      display: flex;
+      flex-flow: row;
+      align-items: center;
+      font-size: 1.2em;
+      letter-spacing: 0.05em;
 
       .logo {
-        width: 1.5em;
+        width: 2.3em;
         padding: 1em;
+        vertical-align: middle;
+      }
+    }
+
+    @media (min-width: 420px) {
+      .title {
+        font-size: 1.4em;
+      }
+    }
+
+    @media (min-width: 576px) {
+      .title {
+        font-size: 2em;
+
+        .logo {
+          width: 1.5em;
+          padding: 1em;
+        }
       }
     }
   }
