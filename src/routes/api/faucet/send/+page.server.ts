a0582617cc13b2042efee151cca50611b4dde03f
Pascallo <pascal.wegener@student.hs-anhalt.de>
2023-05-10 23:58:39 +0200

    tell me how to use cookies

---
+++ src/routes/api/faucet/send/+page.server.ts
@@ -0,0 +1,9 @@
+import { fail, redirect } from '@sveltejs/kit'
+import type { Action, Actions } from './$types'
+
+
+// const login: Action = async ({ cookies, request }) => {
+//     console.log(cookies.getAll())
+// }
+
+// export const actions: Actions = { login }
