3574c5dec2107e35a25b83ee8ff226f7dc49d880
Pascallo <pascal.wegener@student.hs-anhalt.de>
2023-05-10 23:55:44 +0200

    add claimTime functions to faucet load function

--- src/routes/faucet/+page.server.ts
+++ src/routes/faucet/+page.server.ts
@@ -1,11 +1,28 @@
 import { claims } from '$/db/claims'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async function () {
-    const data = await claims.find({}, { limit: 10, projection: { _id: 0 } }).toArray()
-    console.log('data ', data)
-    return {
-        claims: data
+export const load = (async ({ cookies, getClientAddress }) => {
+    const getSomeDbClaims = async () => {
+        const dbClaims = await claims.find({}, { limit: 10, projection: { _id: 0 } }).toArray()
+        return {
+            claims: dbClaims
+        }
     }
-}
 
+    const getClaimTime = () => {
+        const ct = cookies.get('ct')
+        return {
+            ct: ct
+        }
+    }
+
+    const setClaimTime = () => {
+        cookies.set('ct', Date.now().toString(), { path: '/' })
+    }
+
+    return {
+        someClaims: getSomeDbClaims(),
+        lastClaimTime: getClaimTime(),
+        setClaimTime: setClaimTime(),
+    }
+}) satisfies PageServerLoad
